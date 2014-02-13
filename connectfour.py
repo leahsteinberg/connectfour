05acb94b26f1fcb91593e1fd17b091684e7c32bf
leahsteinberg <leahsteinberg1@gmail.com>
2014-02-13 17:22:40 -0500

    works but doesn't check end condition

--- connectfour.py
+++ connectfour.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 SIZE = 7
+player_dict = {'EMPTY': 0, 'BLUE': -1, 'RED': 1}
 EMPTY = 0
-BLUE = 1
-RED = 2
+BLUE = -1
+RED = 1
 board = [[EMPTY]*SIZE for i in range(0, SIZE)]
-
+whose_turn = 'BLUE'
 
 def pretty_print(board):
 	board_string = '0 1 2 3 4 5 6\n'
@@ -19,13 +20,61 @@ def pretty_print(board):
 			board_string+=' '
 		board_string+='\n'
 	return board_string
+
+
+def get_input():
+	while(True):
+		try:
+			column = int(raw_input("player " + whose_turn + "'s turn: "))
+			if column >= 0 and column <= 6:
+				row = 0
+				for index in range(0, SIZE):
+					row = index
+					if board[column][index] != EMPTY:
+						row = index - 1
+						#if column already full
+						if (index-1) < 0:
+							raise ValueError
+						else:
+							break
+				# done gathering info from this player
+				return [column, row]
+			else:
+				raise ValueError
+		except ValueError:
+			print 'give me a different value.'
 	
 
+def turn():
+	global whose_turn
+	move = get_input()
+	update_board(move)
+	print pretty_print(board)
+	if whose_turn == 'BLUE':
+		whose_turn = 'RED'
+	else:
+		whose_turn = 'BLUE'
 
 
+def update_board(move):
+	board[move[0]][move[1]] = player_dict[whose_turn]
+
+
+
+
+	#at the end of this, we switch whose turn it is
+
+
+
+## input = check_input()
+# if input == 0:
+# raise exception
 
 
 if __name__ == "__main__":
-	print pretty_print(board)
+	while(True):
+		turn()
+
+	#print pretty_print(board)
 
 	
