da904e5a82f2c0f3ed83920d9d3657a612f9f350
leahsteinberg <leahsteinberg1@gmail.com>
2014-02-13 15:54:56 -0500

    prints out board state

--- connectfour.py
+++ connectfour.py
@@ -1,4 +1,4 @@
-!#/usr/bin/env python
+#!/usr/bin/env python
 SIZE = 7
 EMPTY = 0
 BLUE = 1
@@ -7,14 +7,25 @@ board = [[EMPTY]*SIZE for i in range(0, SIZE)]
 
 
 def pretty_print(board):
+	board_string = '0 1 2 3 4 5 6\n'
 	for i in range(0, SIZE):
 		for column in board:
-			column[i]
-		#append new line /n
-	#return string
+			if column[i] == EMPTY:
+				board_string+='_'
+			if column[i] == BLUE:
+				board_string+='X'
+			if column[i] == RED:
+				board_string+='O'
+			board_string+=' '
+		board_string+='\n'
+	return board_string
 	
 
 
-def main():
+
+
+
+if __name__ == "__main__":
+	print pretty_print(board)
 
 	
